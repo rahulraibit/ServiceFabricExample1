f9e75cd13831ca4ba6c12a0a4562e33b111e3bdd
h188752 <rahul.rai@honeywell.com>
2017-06-30 22:58:47 +0530

    Added basic service fabric application 1. Created stateLess WebApi 2. Created StateFul WebApi

---
+++ ShipStatelessAPI/ShipStatelessAPI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Description;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Owin;
+
+namespace ShipStatelessAPI
+{
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class ShipStatelessAPI : StatelessService
+    {
+        public ShipStatelessAPI(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            //return new ServiceInstanceListener[0];
+            var endpoints =
+                Context.CodePackageActivationContext.GetEndpoints()
+                    .Where(
+                        endpoint =>
+                            endpoint.Protocol == EndpointProtocol.Http || endpoint.Protocol == EndpointProtocol.Https)
+                    .Select(endpoint => endpoint.Name);
+            return
+                endpoints.Select(
+                    endpoint =>
+                        new ServiceInstanceListener(initParams =>
+                                new OwinCommunicationListener( "api", appbuilder => new Startup().Configuration(appbuilder), initParams), endpoint));
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service instance.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service instance.</param>
+
+    }
+}
---
+++ ShipStatelessAPI/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+namespace ShipStatelessAPI
+{
+    public class Startup
+    {
+        public static void ConfigureFormatters(MediaTypeFormatterCollection formatters)
+        {
+            formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            ConfigureFormatters(config.Formatters);
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
